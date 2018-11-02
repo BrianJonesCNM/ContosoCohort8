cc62d0cdb8042e7cd3df94ea84254d46edc71360
Brian Jones <Brian.Jones@egov.com>
2018-11-02 11:18:52 -0600

    finished task 7 read related date

---
+++ ContosoUniversity/ContosoUniversity/Controllers/CoursesController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public CoursesController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Courses
+        public async Task<IActionResult> Index(string sortOrder, string searchString, string currentFilter,
+            int? page)
+        {
+            ViewData["IDSortParam"] = sortOrder;
+            ViewData["TitleSortParam"] = sortOrder == "Title" ? "title_desc" : "Title";
+            ViewData["CreditSortParam"] = sortOrder == "Credit" ? "credit_desc" : "Credit";
+            ViewData["CurrentFilter"] = searchString;
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            var courses = from c in _context.Courses
+                          //join d in _context.Departments on c.DepartmentID equals d.DepartmentID
+                          select c;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                courses = courses.Where(y => y.Title.Contains(searchString));
+
+            }
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    courses = courses.OrderByDescending(y => y.Title);
+                    break;
+                case "CourseID":
+                    courses = courses.OrderBy(y => y.CourseID);
+                    break;
+                case "courseID_desc":
+                    courses = courses.OrderByDescending(y => y.CourseID);
+                    break;
+                default:
+                    courses = courses.OrderBy(y => y.Title);
+                    break;
+            }
+            int pageSize = 3;
+
+            //get the paginated list back with the courses we need
+            var coursesList = await PaginatedList<Course>.CreateAsync(courses.AsNoTracking(), page ?? 1, pageSize);
+            //Loop through the courses and get thier department
+            foreach(Course course in coursesList)
+            {
+                course.Department = _context.Departments.SingleOrDefault(y => y.DepartmentID == course.DepartmentID);
+            }
+
+            return View(coursesList);
+        }
+
+        // GET: Courses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .SingleOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Courses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CourseID,Title,Credits")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return View(course);
+        }
+
+        // POST: Courses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CourseID,Title,Credits")] Course course)
+        {
+            if (id != course.CourseID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.CourseID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .SingleOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.CourseID == id);
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.CourseID == id);
+        }
+    }
+}
